3576e01322ef1ac3cabedf1316e33d8cfd037956
jkz_zh <jkz_zh@sina.com>
2015-12-17 16:37:40 +0800

    update

---
+++ JKCycleScrollView/JKCycleScrollViews/Views/JKCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  JKCollectionViewCell.h
+//  JKCycleScrollView
+//
+//  Created by Kzj_ZhongJunKang on 15/12/15.
+//  Copyright © 2015年 Kzj_ZhongJunKang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) UIImageView *imageView;
+@property (copy, nonatomic) NSString *title;
+@end
